62fd277f5f025a4958a6d35f493585b9bc8a023e
Keval navadiya <31183263+kk-007@users.noreply.github.com>
2020-07-28 01:44:44 +0530

    changed bundle directly

--- bundle.js
+++ bundle.js
@@ -7390,7 +7390,10 @@ class Peer extends stream.Duplex {
   _onConnectionStateChange () {
     if (this.destroyed) return
     if (this._pc.connectionState === 'failed') {
-      this.destroy(makeError('Connection failed.', 'ERR_CONNECTION_FAILURE'))
+      //this.destroy(makeError('Connection failed.', 'ERR_CONNECTION_FAILURE'))
+      console.log('ERR_CONNECTION_FAILURE');
+      if (this._connected)
+        this.destroy(makeError('Connection failed.', 'ERR_CONNECTION_FAILURE'));
     }
   }
 
@@ -7411,7 +7414,10 @@ class Peer extends stream.Duplex {
       this._maybeReady()
     }
     if (iceConnectionState === 'failed') {
-      this.destroy(makeError('Ice connection failed.', 'ERR_ICE_CONNECTION_FAILURE'))
+      //this.destroy(makeError('Ice connection failed.', 'ERR_ICE_CONNECTION_FAILURE'))
+      console.log('ERR_ICE_CONNECTION_FAILURE');
+      if (this._connected)
+        this.destroy(makeError('Ice connection failed.', 'ERR_ICE_CONNECTION_FAILURE'));
     }
     if (iceConnectionState === 'closed') {
       this.destroy(makeError('Ice connection closed.', 'ERR_ICE_CONNECTION_CLOSED'))
